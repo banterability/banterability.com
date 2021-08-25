c936b68c9428dae4d7854fac8a2c3c61bd0eb987
Jeff Long <banterability@users.noreply.github.com>
2021-08-25 11:42:10 -0500

    prettier

--- .eleventy.js
+++ .eleventy.js
@@ -1,41 +1,45 @@
-const {DateTime} = require('luxon');
+const { DateTime } = require("luxon");
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
-module.exports = function(eleventyConfig) {	
-	eleventyConfig.addPlugin(eleventyNavigationPlugin);
-	// eleventyConfig.setDataDeepMerge(true);
-	
-	eleventyConfig.addFilter("readableDate", dateObj => {
-		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
-	  });
-	
-	  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-	  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-	  });
-	
-	  // Get the first `n` elements of a collection.
-	  eleventyConfig.addFilter("head", (array, n) => {
-		if(!Array.isArray(array) || array.length === 0) {
-		  return [];
-		}
-		if( n < 0 ) {
-		  return array.slice(n);
-		}
-	
-		return array.slice(0, n);
-	  });
-	
-	  // Return the smallest number argument
-	  eleventyConfig.addFilter("min", (...numbers) => {
-		return Math.min.apply(null, numbers);
-	  });
-	  function filterTagList(tags) {
-		  return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
-		}
-	  
-		eleventyConfig.addFilter("filterTagList", filterTagList)
-	
-	  eleventyConfig.addPassthroughCopy("fonts");
-	  eleventyConfig.addPassthroughCopy("styles");
-	};
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  // eleventyConfig.setDataDeepMerge(true);
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
+      "dd LLL yyyy"
+    );
+  });
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
+  });
+
+  // Get the first `n` elements of a collection.
+  eleventyConfig.addFilter("head", (array, n) => {
+    if (!Array.isArray(array) || array.length === 0) {
+      return [];
+    }
+    if (n < 0) {
+      return array.slice(n);
+    }
+
+    return array.slice(0, n);
+  });
+
+  // Return the smallest number argument
+  eleventyConfig.addFilter("min", (...numbers) => {
+    return Math.min.apply(null, numbers);
+  });
+  function filterTagList(tags) {
+    return (tags || []).filter(
+      (tag) => ["all", "nav", "post", "posts"].indexOf(tag) === -1
+    );
+  }
+
+  eleventyConfig.addFilter("filterTagList", filterTagList);
+
+  eleventyConfig.addPassthroughCopy("fonts");
+  eleventyConfig.addPassthroughCopy("styles");
+};
